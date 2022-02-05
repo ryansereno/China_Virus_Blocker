b0323cb9a334bba97dac695ad6c94c85930b98b3
ryansereno <ryansereno@protonmail.com>
2022-02-05 14:52:43 -0800

    added replaceImage function to block images; added additional keywords to be blocked

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Please play while contributing: https://www.youtube.com/watch?v=ZPhdpk5Rd6A
 
-let blacklist = ['coronavirus', 'COVID-19', 'Omicron', 'booster', 'vaccine', 'N95', 'Quarantine', 'CDC', 'lockdown', 'covid', 'delta variant', 'pandemic'];
+let blacklist = ['coronavirus', 'COVID-19', 'Omicron', 'booster', 'vaccine', 'vaccinated', 'vaccination', 'N95', 'Quarantine', 'CDC', 'lockdown', 'covid', 'delta variant', 'pandemic', 'mask', 'masked'];
 let blacklistStr = blacklist.join('|');
 let reg = new RegExp(blacklistStr, 'gi');
 
-replaceText(document.body)
+replaceText(document.body);
+replaceImage(document.body);
 
 // ***
 function replaceText(element){
@@ -12,10 +13,18 @@ function replaceText(element){
 
         //loop through element hierarchy until text element/ non child element is found
         element.childNodes.forEach(replaceText) 
-
     }   else if (element.nodeType === Text.TEXT_NODE && element.textContent.match(reg)) {
+            element.parentElement.style.color = 'black'
+            element.parentElement.style.backgroundColor = 'black'
+    }
+}
 
-        element.parentElement.style.color = 'black'
-        element.parentElement.style.backgroundColor = 'black'
+function replaceImage(element) {
+        console.log(element.nodeName)
+    if (element.hasChildNodes()){
+        element.childNodes.forEach(replaceImage)
+    }   else if (element.nodeName == "IMG" && (element.src).match(reg)){  // || JSON.stringify(element.alt).match(reg)) {
+            console.log(JSON.stringify(element.alt));
+            element.style.visibility = "hidden";
     }
 }
