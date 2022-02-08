f94747febd39f0d0018bd3a2a1922a90fdca8b6c
ryansereno <ryansereno@protonmail.com>
2022-02-07 19:13:11 -0800

    added replaceAttribute() function to help with latent image blocking

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ let blacklist = ['coronavirus', 'COVID-19', 'Omicron', 'booster', 'vaccine', 'va
 let blacklistStr = blacklist.join('|');
 let reg = new RegExp(blacklistStr, 'gi');
 
+replaceAttribute()
 replaceContent(document.body);
+
+function replaceAttribute(){
+    var imgs = document.getElementsByTagName('img');
+    var imgs_size = imgs.length;
+    var url;
+
+    for (var i = 0; i < imgs_size; i++) {
+        var el = imgs[i];
+
+        if (url = el.getAttribute('data-original')) {
+            el.setAttribute('src', url);
+        } else if (url = el.getAttribute('data-src')) {
+            el.setAttribute('src', url);
+        } else if (url = el.getAttribute('file')) {
+            el.setAttribute('src', url);
+        } 
+    }
+}
 
 // ***
 function replaceContent(element){
@@ -16,6 +35,8 @@ function replaceContent(element){
             element.parentElement.style.color = "white"
             element.parentElement.style.backgroundColor = "white"
     }   else if (element.nodeName == "IMG" && (element.src).match(reg)){  // || JSON.stringify(element.alt).match(reg)) {
+        element.style.visibility = "hidden";
+    }   else if (element.nodeName == "IMG" && (JSON.stringify(element.alt)).match(reg)){  // || JSON.stringify(element.alt).match(reg)) {
         console.log(JSON.stringify(element.alt));
         element.style.visibility = "hidden";
     }
